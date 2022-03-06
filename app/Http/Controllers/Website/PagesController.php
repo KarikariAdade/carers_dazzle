97dc8a37b4d6de0cc85783ecd4a28093ea8a5439
ria-hue <mumuninasaria@gmail.com>
2022-03-06 21:17:13 +0000

    initial commit

---
+++ app/Http/Controllers/Website/PagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Website;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function shop()
+    {
+        return view('website.shop.index');
+    }
+
+
+    public function category()
+    {
+        return view('website.category.index');
+    }
+
+    public function brand()
+    {
+        return view('website.brand.index');
+    }
+
+    public function productDetail()
+    {
+        return view('website.shop.detail');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Website\PagesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('website.home');
+
+#========================================= WEBSITE ROUTES ======================================#
+
+Route::get('category', [PagesController::class, 'category'])->name('website.category.index');
+Route::get('brand', [PagesController::class, 'brand'])->name('website.brand.index');
+Route::get('product/detail', [PagesController::class, 'productDetail'])->name('website.product.detail');
+Route::get('shop', [PagesController::class, 'shop'])->name('website.shop.index');
